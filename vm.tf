a33bb13a8b726a5bac009e370679dfc63c7b2e13
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-24 23:30:41 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -13,10 +13,25 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
 }
 
 # 🔹 Create a Resource Group
